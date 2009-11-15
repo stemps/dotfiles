76b6e61a691e7ebe23741d315dfeef9946f8a7a6
Simon Stemplinger <simon.stemplinger@gmx.de>
2009-11-15 15:51:21 +0800

    initial commit

---
+++ ooffice.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/Applications/NeoOffice.app/Contents/MacOS/soffice "-accept=socket,host=localhost,port=2002;urp;" &
---
+++ sync-omnifocus-ical.applescript
@@ -0,0 +1,1 @@
+tell front document of application "OmniFocus" to synchronize
